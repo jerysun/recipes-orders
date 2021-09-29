ceb62798482fc492381217a56fab594bda39a383
Jerry Sun <jerry.sun@eonics.nl>
2021-09-29 22:07:27 +0200

    clicking recipe-item triggers recipe-detail

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import {Recipe} from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
     new Recipe(
       'A Test Recipe',
@@ -13,7 +16,7 @@ export class RecipeListComponent implements OnInit {
       'https://assets.bonappetit.com/photos/5d7296eec4af4d0008ad1263/16:9/w_1600,c_limit/Basically-Gojuchang-Chicken-Recipe-Wide.jpg'
     ),
     new Recipe(
-      'A Test Recipe',
+      'Another Test Recipe',
       'This is simply a test',
       'https://assets.bonappetit.com/photos/5d7296eec4af4d0008ad1263/16:9/w_1600,c_limit/Basically-Gojuchang-Chicken-Recipe-Wide.jpg'
     ),
@@ -22,6 +25,10 @@ export class RecipeListComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
