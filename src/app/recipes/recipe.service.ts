e699036bbdd93d2b3a2c222466a1c58661960361
Jerry Sun <jerysun007@hotmail.com>
2021-12-21 18:47:31 +0100

    Added the interaction with backend via Http

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -10,10 +10,13 @@ import { Recipe } from './recipe.model';
 export class RecipeService {
   recipeChanged = new Subject<Recipe[]>();
 
+  /*
   private recipes: Recipe[] = [
     new Recipe('Tasty Schnitzel', 'A super-tasty Schnitzel - just awesome!', 'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG', [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]),
     new Recipe('Big Fat Burger', 'What else you need to say?', 'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg', [new Ingredient('Buns', 2), new Ingredient('Meat', 2)]),
   ];
+   */
+  private recipes: Recipe[] = [];
 
   constructor(private slService: ShoppingListService) { }
 
@@ -31,6 +34,11 @@ export class RecipeService {
 
   addRecipe(recipe: Recipe): void {
     this.recipes.push(recipe);
+    this.recipeChanged.next(this.recipes.slice());
+  }
+
+  setRecipes(recipes: Recipe[]): void {
+    this.recipes = recipes;
     this.recipeChanged.next(this.recipes.slice());
   }
 
